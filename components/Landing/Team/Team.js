52e38b6b9b0f6783f44b183d73c1765072b48521
Pankaj <pankaj.k@amakein.com>
2021-03-19 14:55:55 +0530

    assa

---
+++ components/Landing/Team/Team.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import  { Section } from './Style'
+import Image from 'next/image';
+function Team() {
+    return (
+        <Section>
+            <div className="head" >
+                <h2>The Team Behind Good Vibes</h2>
+            </div>
+
+            <div  className="grid" >
+             <div className="left" >
+
+             <Image
+            className="myImage"
+           src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1616143872/goodVibes/nccoxgnjwymeclvvcoln.png'
+           alt="Picture of the author"
+           layout="fill"
+           objectFit="cover"
+           />   
+             </div>
+             <div className="right" >
+    
+    <div className="top" >
+    We’re an experienced and very creative production agency
+    </div>
+    <div className="mid" >
+    Build on a culture of contribution and inclusion is a lot of persent cross-pollination, nor closing these latest prospects is like putting socks on an octopus, yet circle back. We need to make the new version clean and sexy critical mass.
+    </div>
+   
+             </div>
+             
+             
+             
+             
+             </div>
+        </Section>
+   
+   )
+}
+
+export default Team
