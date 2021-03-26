7b0f7f1d226c8bdba4b247a5ee9142cc219c2224
Pankaj <pankaj.k@amakein.com>
2021-03-26 15:20:05 +0530

    dssd

--- components/Landing/showreel/Style.js
+++ components/Landing/showreel/Style.js
@@ -53,12 +53,91 @@ justify-content:space-between;
       display:flex;
       justify-content:center;
       align-items:center;
-      flex-direction:column;
+      flex-wrap:wrap;
+      
       .data {
-          width:100%;
+          width:33%;
           height:50%;
           position: relative;
+          display:flex;
+          justify-content:center;
+          align-items:center;
+          .cover {
+            width:99.5%;
+            height:99.5%;
+          }
+          .img {
+            position:absolute;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            width:100%;
+            height:100%;
+   
+            img {
+              width:20%;
+            }
+
+
+          }
+
+          .img:after {
+  display:block;
+  position: absolute;
+  width:100%;
+
+bottom:0%;
+  content: '';
+  border-bottom: solid 8px #D0345A;  
+  transform: scaleX(0);  
+  transition: transform 1s ease-in-out;
+}
+.img:hover:after {
+        transform: scaleX(1);
       }
+
+          .modal {
+ /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 5; /* Sit on top */
+  padding-top: 2%; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background: rgba(0, 0, 0, 0.95);
+background: rgba(0, 0, 0, 0.95); /* Black w/ opacity */
+  .modal-content {
+    position: relative;
+ 
+  margin: auto;
+  margin-top:10rem;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 50%;
+  height:50%;
+  border-radius:10px;
+  min-height:10rem;
+    display:flex;
+    justify-content:space-evenly;
+    align-items:center;
+    flex-direction:column;
+      
+    i {
+      position:absolute;
+      right:-5%;
+      color:white;
+      top:2%;
+      font-size:2rem;
+      cursor: pointer;
+    }
+}
+}
+      }
+
+      
+      
   }
     }
   
--- components/Landing/showreel/showreel.js
+++ components/Landing/showreel/showreel.js
@@ -1,7 +1,58 @@
 import React from 'react'
 import { Section } from './Style'
 import Image from 'next/image';
+import ReactPlayer from 'react-player';
+import { useEffect  , useState  } from 'react';
+const data = [
+     
+    {
+        _id:"1",
+ titile:"dssd",
+link:'cNoFX4mWCgA',
+
+
+    },
+    {
+        _id:"2",
+        titile:"dssd",
+       link:"MIX2893GWDw",
+    },
+    {
+        _id:"3",
+        titile:"dssd",
+       link:"W9Nh-WxW3A0",
+    },
+
+    {
+        _id:"4",
+        titile:"dssd",
+       link:"pPWyijx4Z_A",
+    },
+    {
+        _id:"5",
+        titile:"dssd",
+       link:"kUU55unfvyQ",
+    },
+    {
+        _id:"6",
+        titile:"dssd",
+       link:"cNoFX4mWCgA",
+    },
+
+    
+
+
+]
+
 function showreel() {
+     
+    const [  visible , setvisible] = useState(false);
+    const toggleMenu = id => {
+      setvisible(id)
+  }
+  
+
+
     return (
         <Section>
          <div className="head" >
@@ -25,25 +76,42 @@ function showreel() {
   </div>
 
   <div className="list" >
-   <div className="data" >
-   <Image
-            className="myImage"
-           src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1616134163/goodVibes/rcj9q0rcbqbqtenv2kdh.png'
-           alt="Picture of the author"
-           layout="fill"
-           objectFit="cover"
-           />   
-   </div>
-   <div className="data" >
-   <Image
-            className="myImage"
-           src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1616134141/goodVibes/yp8powk5zcxsjfrsokla.png'
-           alt="Picture of the author"
-           layout="fill"
-           objectFit="cover"
-           />   
-   </div>
+    {data.map((number) => 
+        <div  key={number._id} className="data" >
+     <img  className="cover"  src={`https://img.youtube.com/vi/${number.link}/0.jpg`} ></img>
+    <div className="img" >
+        <img onClick={() => toggleMenu(number._id) }  src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1616750466/auegoewnepob301zsbj8.svg" ></img>
+         </div>
 
+         {visible === number._id &&
+      
+      <div id="myModal" class="modal">
+
+
+<div class="modal-content">
+<ReactPlayer
+           volume={0} 
+           className='react-player'
+       
+           volume={100}
+  url={`https://www.youtube.com/watch?v=${number.link}`}
+  playing={true}
+  loop={true} 
+  width='100%'
+  height='100%'
+/>
+    
+<i onClick={()  => setvisible(false)} class="fas fa-times"></i>
+     
+     </div> 
+
+     </div>
+
+   }
+  </div>
+    )}
+   
+ 
   </div>
 
          </div>
