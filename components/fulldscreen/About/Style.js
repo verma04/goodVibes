6ea0abf3fb58d0bfcf5cb98478a460357a7f5bf4
Pankaj <pankaj.k@amakein.com>
2021-04-16 11:13:40 +0530

    dssdds

--- components/fulldscreen/About/Style.js
+++ components/fulldscreen/About/Style.js
@@ -49,4 +49,40 @@ justify-content:space-around;
   
 
 }
+
+
+@media (min-width: 768px) and (max-width: 1024px) {
+  
+    
+  .flex-1 {
+      width:60%;
+      display:flex;
+justify-content:space-around;
+ align-items:center;
+  flex-direction:column;
+  height:100%;
+  h3 {
+      text-align:center;
+      font-weight:600;
+      font-size:1.8rem;
+      font-family: Mono_bold;
+      padding-top: 5rem;
+    padding-bottom:2rem;
+  }
+  p {
+    text-align:center;
+  
+ 
+    word-spacing:0.4rem;
+  }
+
+     
+    }
+  
+ 
+
+    
+  
+
+}
 `
