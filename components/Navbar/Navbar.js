cf87b5a3f0e3ec04788513e1df31ee3fe44541e3
Pankaj <pankaj.k@amakein.com>
2021-04-16 16:17:19 +0530

    dssd

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -158,7 +158,7 @@ onClick={ () => setvisible(false)}
              </div>
              <div   onClick={toggleTheme} className="nav-item" >
       
-             {(() => {
+             {/* {(() => {
 if (theme === "light") {
 return (
  <div  className='mode' onClick={toggleTheme} >  
@@ -172,7 +172,12 @@ return (
     </div>
 )
 }
-})()}
+})()} */}
+
+<ToggleContainer lightTheme={isLight} onClick={toggleTheme}>
+      <img src="https://image.flaticon.com/icons/svg/1164/1164954.svg" width="224" height="224" alt="Sun free icon" title="Sun free icon"/>
+      <img src="https://image.flaticon.com/icons/svg/2033/2033921.svg" width="224" height="224" alt="Moon free icon" title="Moon free icon"/>
+    </ToggleContainer>
 
 
 
