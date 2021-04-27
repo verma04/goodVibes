005ff91453e1a8acba137f09db4f8f3cf7161fa6
Pankaj <pankaj.k@amakein.com>
2021-04-27 11:46:10 +0530

    wee

--- components/Landing/partner/partner.js
+++ components/Landing/partner/partner.js
@@ -1,12 +1,187 @@
-import React from 'react'
+import React , {useEffect , useState} from 'react'
 import { Section } from './Style'
 import Image from 'next/image';
 function partner() {
+        const [state, setstate] = useState("")
+        useEffect(function() {
+                
+                  setstate(window.localStorage.theme)
+            },[]);
+  if(state === "dark") {
+  return (
+  <Section>
+  <div className="head" >
+<div  className="left" > 
+<h5>Our Clients  </h5>
+<p> We’re going to
+became partners
+for the long run.</p>
+</div>
+<div  className="right" >
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.  The point of using <span> Lorem Ipsum is that it has a more-or-less normal</span> distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+</div>
+  </div>
+  
+  <div data-aos="fade-down" className="data" >
+      <div className="partner" >
+      <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1619503291/pccqrnbtedh5eybzklr3.png'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+      </div>
+
+      <div className="partner" >
+      <Image
+  className="myImage"
+ src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1619503998/bc97dsfymuqbpkarphn3.png"
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617258182/goodVibes/wsyijcfuowu06czre4wx.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617258182/goodVibes/vdf16gtfuuqgx6rluugr.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617258182/goodVibes/gsakjmhuvuet1ldzv3zw.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260065/goodVibes/c4zf5d3iwqpvq3onabw8.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1619503291/pccqrnbtedh5eybzklr3.png'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260065/goodVibes/u4nf7lxjtxa6k4qzavfl.jpg"
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260065/goodVibes/jqrck9fw3nfuywrjr1qy.jpg"
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260066/goodVibes/nqpnclcb1oegcde8smgl.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260066/goodVibes/z76zdjc339jdxxy4yzsj.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ />   
+          </div>
+          
+         
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260066/goodVibes/nwo0bam38fg0ou20npqg.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+ </div>
+       
+          <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260066/goodVibes/kaphn9ty9skwtg3c3yht.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+  </div>
+  <div className="partner" >
+          <Image
+  className="myImage"
+ src='https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260066/goodVibes/mubea31erup0ytistwdw.jpg'
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+          </div>
+  <div className="partner" >
+          <Image
+  className="myImage"
+ src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1617260083/goodVibes/tctkkrjbaucuy5cuqwp1.jpg"
+ alt="Picture of the author"
+ layout="fill"
+ objectFit="cover"
+ /> 
+ </div>
+  </div>
+
+  <div data-aos="zoom-out" className="button" >
+      <button  >Viddew More</button>
+  </div>
+</Section>
+  )
+  }
+
+  else {
     return (
+ 
+
         <Section>
             <div className="head" >
    <div  className="left" > 
-    <h5>Our Clients</h5>
+    <h5>Our Clients  </h5>
     <p> We’re going to
 became partners
 for the long run.</p>
@@ -30,7 +205,7 @@ for the long run.</p>
                 <div className="partner" >
                 <Image
             className="myImage"
-           src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1617802629/u3jvyn6iwz1br78bcrq7.png"
+           src="https://res.cloudinary.com/dzcmadjl1/image/upload/v1619503998/zegtqmj2gh1nqwcukqpe.png"
            alt="Picture of the author"
            layout="fill"
            objectFit="cover"
@@ -165,7 +340,10 @@ for the long run.</p>
                 <button  >View More</button>
             </div>
         </Section>
+    
+    
     )
+  }
 }
 
 export default partner
